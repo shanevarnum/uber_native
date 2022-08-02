1e75b06d69df06cb33fbef4bd40ee5f97f0c9ff4
Shane Varnum <svarnum@64labs.com>
2022-08-02 17:07:47 -0400

    added more styling and maps functionality

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import { FlatList, StyleSheet, Text, Image, TouchableOpacity, View } from 'react
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [
     {
@@ -21,6 +23,7 @@ const data = [
 
 const NavOptions = () => {
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
 
 
     return (
@@ -33,13 +36,16 @@ const NavOptions = () => {
                     onPress={() => navigation.navigate(item.screen)}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
                 >
-                    <View>
+                    <View style={tw`${!origin && "opacity-20"}`}>
                         <Image 
                             style={{ width: 120, height: 120, resizeMode: 'contain'}}
                             source={{ uri: item.image }}
                         />
                         <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                        <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name="arrowright" color="white" type='antdesign' />
+                        <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} 
+                            name="arrowright" 
+                            color="white" 
+                            type='antdesign' />
                     </View>
                 </TouchableOpacity>    
             )}
---
+++ components/NavigateCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from "@env"
+
+const NavigateCard = () => {
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+        <Text style={tw`text-center py-5 text-xl`}>Good Morning, Shane</Text>
+        <View style={tw`border-t border-gray-200 flex-shrink`}>
+            <GooglePlacesAutocomplete 
+                placeholder='Where to?'
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                minLength={2}
+                enablePoweredByContainer={false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    anguage: "en",
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+            />
+        </View>
+    </SafeAreaView>    
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
+});
