a949296998d4b30aff810a7d127181bc80dbbdc4
Shane Varnum <svarnum@64labs.com>
2022-08-04 16:10:49 -0400

    wired up distance and pricing calculations

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,6 +3,8 @@ import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectTravelTimeInformation } from '../slices/navSlice';
 
 const data = [
   {
@@ -13,7 +15,7 @@ const data = [
   },
   {
     id: "Uber-XL-456",
-    title: "Uber-XL",
+    title: "Uber XL",
     multiplier: 1.2,
     image: "https://links.papareact.com/5w8",
   },
@@ -25,9 +27,12 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation)
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -38,7 +43,7 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride - {travelTimeInformation?.distance.text}</Text>
       </View>
       <FlatList 
         data={data}
@@ -60,12 +65,29 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travle time...</Text>
+              <Text>{travelTimeInformation?.duration.text} Travel Time</Text>
             </View>
-            <Text style={tw`text-xl`}>$99</Text>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat('en-us', {
+                style: 'currency',
+                currency: 'USD'
+              }).format(
+                (travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier / 100)
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
+
+      <View>
+        <TouchableOpacity 
+          disabled={!selected} 
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
